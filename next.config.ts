826db564ad21440f11f54aded948ebf2776c1288
Sophiram <sornsophiram11@gmail.com>
2026-06-04 10:59:18 +0700

    fix: remove static output config for api routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: 'export', // 👈 បន្ថែមបន្ទាត់នេះ
 };
 
 export default nextConfig;
