2dc9f7dc338f7b895017a6f383df5f81202a9d91
Sophiram <sornsophiram11@gmail.com>
2026-06-04 11:13:48 +0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function LoginPage() {
             <input
               type="text"
               required
-              value={username}
+              value={"admin"}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full px-4 py-3 bg-slate-50 border border-slate-200 rounded-xl focus:outline-none focus:border-blue-500 font-medium transition text-sm"
               placeholder="បញ្ចូលឈ្មោះអ្នកប្រើប្រាស់"
@@ -45,7 +45,7 @@ export default function LoginPage() {
             <input
               type="password"
               required
-              value={password}
+              value={"admin123"}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full px-4 py-3 bg-slate-50 border border-slate-200 rounded-xl focus:outline-none focus:border-blue-500 font-medium transition text-sm"
               placeholder="បញ្ចូលលេខសម្ងាត់"
